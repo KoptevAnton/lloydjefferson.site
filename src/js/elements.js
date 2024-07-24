6a9c7548755746777e9efb90d61c3050ea60f2cf
Serhii Reshetniak <reshet@lozovaya.org.ua>
2024-07-24 19:19:38 +0300

    Prepare to the work * Update README * Update build command in package.json * Update styles.css to import modern-normalize and font, update defaults * Update index.html and add HTML and CSS files for each section * Remove all defaults including images and add logo as example * Update favicon.ico * Add elements.js as example into js folder

---
+++ src/js/elements.js
@@ -0,0 +1,13 @@
+/**
+ * Import to your file:
+ * import elements from './elements'
+ *
+ * Example usage:
+ * elements.form.addEventListener('submit', (event) => {
+ *   event.preventDefault();
+ * });
+ */
+
+export default {
+  form: document.querySelector('.form'),
+};
