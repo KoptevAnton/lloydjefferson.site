609352037bda08293208bfa8fb482708a1e44bac
Kseniya <159527696+Kseniya-Igorivna@users.noreply.github.com>
2024-07-29 10:54:18 +0300

    add

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,39 +1,35 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-document.addEventListener('DOMContentLoaded', () => {
-    const faqItems = document.querySelectorAll('.faq-list-ul .list-faq');
+import { faqItems } from './elements.js';
 
-    faqItems.forEach(item => {
-        const question = item.querySelector('.title-list');
-        const toggleBtn = item.querySelector('.toggle-btn');
-        const answer = item.querySelector('.text-faq');
-        const iconOpen = toggleBtn.querySelector('.icon-open');
-        const iconClose = toggleBtn.querySelector('.icon-close');
-        
+document.addEventListener('DOMContentLoaded', () => {
+  faqItems.forEach(item => {
+    const question = item.querySelector('.title-list');
+    const toggleBtn = item.querySelector('.toggle-btn');
+    const answer = item.querySelector('.text-faq');
+    const iconOpen = toggleBtn.querySelector('.icon-open');
+    const iconClose = toggleBtn.querySelector('.icon-close');
+
+    answer.style.display = 'none';
+    item.setAttribute('aria-expanded', 'false');
+    iconOpen.style.display = 'block';
+    iconClose.style.display = 'none';
+
+    question.addEventListener('click', () => {
+      const isOpen = answer.style.display === 'block';
+
+      if (!isOpen) {
+        answer.style.display = 'block';
+        item.setAttribute('aria-expanded', 'true');
+        iconOpen.style.display = 'none';
+        iconClose.style.display = 'block';
+      } else {
         answer.style.display = 'none';
         item.setAttribute('aria-expanded', 'false');
         iconOpen.style.display = 'block';
         iconClose.style.display = 'none';
-        
-        question.addEventListener('click', () => {
-            const answer = item.querySelector('.text-faq');
-            const iconOpen = toggleBtn.querySelector('.icon-open');
-            const iconClose = toggleBtn.querySelector('.icon-close');
-            const isOpen = answer.style.display === 'block';
-
-            if (!isOpen) {
-                answer.style.display = 'block';
-                item.setAttribute('aria-expanded', 'true');
-                iconOpen.style.display = 'none';
-                iconClose.style.display = 'block';
-            } else {
-                answer.style.display = 'none';
-                item.setAttribute('aria-expanded', 'false');
-                iconOpen.style.display = 'block';
-                iconClose.style.display = 'none';
-            }
-        });
+      }
     });
+  });
 });
-
