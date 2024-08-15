392b114168d8e66e0901e5015194e290ab7aaee2
KoptevAnton <koptev110593@gmail.com>
2024-08-15 03:20:35 +0300

    Prettier.

--- src/js/projects.js
+++ src/js/projects.js
@@ -2,7 +2,7 @@ import Swiper from 'swiper';
 import { Navigation, Keyboard } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import {prevButtonEl, nextButtonEl} from './elements.js'
+import { prevButtonEl, nextButtonEl } from './elements.js';
 
 Swiper.use([Navigation, Keyboard]);
 new Swiper('.projects__swiper-container', {
